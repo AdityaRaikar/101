0780f60a427bd4ae1fe931f985d7b015d75b9716
AdityaRaikar <85438792+AdityaRaikar@users.noreply.github.com>
2024-07-11 13:47:26 +0530

    Add files via upload

---
+++ Car Plate.py
@@ -0,0 +1,10 @@
+import random
+
+response = "y"
+
+while response =="y":
+    no = random.randint(100,999)
+    
+    response = input("To generate new Plate press y or n:")
+    print("KA18 EF",no)
+    
